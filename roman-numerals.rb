36f4fd9cc0f2c6f53daea91430b298091c9837be
Ted Pennings <tedpennings@gmail.com>
2013-08-25 21:29:55 -0400

    slide code cleanup

--- roman-numerals.rb
+++ roman-numerals.rb
@@ -6,31 +6,29 @@
 # ::numeral:: the number to parse
 def numeral_to_string numeral
   raise ArgumentError, "Invalid characters in numeral '#{numeral}'" unless numeral =~ /^[IVXLCDM]+$/i
-  numbers = []
-  numeral.upcase.chars.each do |char|
+  numbers = numeral.upcase.chars.map do |char|
     case char
     when "I"
-      numbers << 1
+      1
     when "V"
-      numbers << 5
+      5
     when "X"
-      numbers << 10
+      10
     when "L"
-      numbers << 50
+      50
     when "V"
-      numbers << 50
+      50
     when "C"
-      numbers << 100
+      100
     when "D"
-      numbers << 500
+      500
     when "M"
-      numbers << 1000
-    else #this check is pretty much unnecessary
-      raise ArgumentError, "Unable to parse #{char} in #{numeral}"
+      1000
     end
   end
   sum = 0
   numbers.each_with_index do |n, i|
+    # check for negative values (subtraction) before summing
     n = n * -1 if !numbers[i+1].nil? && numbers[i+1] > n
     sum += n
   end
@@ -39,7 +37,7 @@ end
 
 def print_it numeral
   translated = numeral_to_string numeral
-  puts "i is #{translated}"
+  puts "#{numeral} is #{translated}"
 end
 
 ["L", "X", "XXIX", "IVXLCDM", "CCXXXIII", "CCCLXXVIII", "XCIX"].each { |i| print_it i }
